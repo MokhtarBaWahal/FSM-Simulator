7c62b478fabad2f20092d9282cf412a354446561
Dana Alkhouri <danakh30@aucegypt.edu>
2024-05-25 22:24:45 +0300

    minor edit

--- WaitForInputCommand.h
+++ WaitForInputCommand.h
@@ -6,27 +6,26 @@
 
 class WaitForInputCommand : public Command {
 public:
-    void execute(FSM* fsm) override {
-        int input;
-        std::cout << "Enter a number for transition: ";
-        std::cin >> input;  // Capture input from the user
+  void execute(FSM *fsm) override {
+    int input;
+    std::cout << "Enter a number for transition: ";
+    std::cin >> input; // Capture input from the user
 
-        fsm->setCurrentInput(input);  // Set the input in the FSM
+    fsm->setCurrentInput(input); // Set the input in the FSM
 
-        // Attempt to transition based on the input
-        std::string nextState = fsm->getTransitionForInput();
-        bool validInput= false;
-        while (nextState.empty()) {
+    // Attempt to transition based on the input
+    std::string nextState = fsm->getTransitionForInput();
+    bool validInput = false;
+    while (nextState.empty()) {
 
-            std::cout << "No valid transition for input: " << input << std::endl;
-            std::cout << "Enter a number for transition: ";
-            std::cin >> input;  // Capture input from the user
+      std::cout << "No valid transition for input: " << input << std::endl;
+      std::cout << "Enter a number for transition: ";
+      std::cin >> input; // Capture input from the user
 
-            fsm->setCurrentInput(input);  // Set the input in the FSM
+      fsm->setCurrentInput(input); // Set the input in the FSM
 
-            nextState= fsm->getTransitionForInput();
-        }
-        fsm->changeStateDirectly(nextState);
-
+      nextState = fsm->getTransitionForInput();
     }
+    fsm->changeStateDirectly(nextState);
+  }
 };
